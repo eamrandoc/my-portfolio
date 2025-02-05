6974fcd1fcb5271032cfaed9f31b5dfe9af77ea0
eamrandoc <eamrandoc@gmail.comm>
2025-02-06 01:00:09 +0600

    just setup basic

---
+++ src/routes/routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <p>This is Error page</p>,
+        children: [
+            {
+                path: '/',
+                element: <p>Home page</p>
+            },
+            
+        ]
+
+    }
+])
+
+export default routes
